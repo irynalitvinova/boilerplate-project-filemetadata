401b6571caf53e0a18020275103bf83bdbc31857
Iryna Litvinova <lytvynlysko@gmail.com>
2023-09-12 18:08:07 +0300

    Add solution to File Metadata Microservice

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const multer = require('multer');
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+const upload = multer({dest: './public/data/upload' });
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  //  name, type, and size
+  const {originalname, mimetype, size} = req.file;
+  // req.file.originalname;
+  // req.file.mimetype;
+  // req.file.size;
+  // res.json(req.file);
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
